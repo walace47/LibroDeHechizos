ffbdbb0e50d677b71a1d2b3c77dac86800409148
Giuliano <giuliano.marinelli@hotmail.com>
2017-03-07 17:35:41 -0300

    Initial commit

---
+++ app/src/main/java/com/joan/librohechizos/ui/CrearPersonaje.java
@@ -0,0 +1,19 @@
+package com.joan.librohechizos.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.joan.librohechizos.R;
+
+public class CrearPersonaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_personaje);
+    }
+
+    public void btnPersonajeCrear() {
+
+    }
+}
