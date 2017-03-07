fe545fa5e3eb90df5d8d74a31ed7b6d5049835c3
Giuliano <giuliano.marinelli@hotmail.com>
2017-03-07 17:41:46 -0300

    Para que ande el boton

--- app/src/main/java/com/joan/librohechizos/ui/ListarPersonajesActivity.java
+++ app/src/main/java/com/joan/librohechizos/ui/ListarPersonajesActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.joan.librohechizos.R;
 import com.joan.librohechizos.modelo.*;
@@ -45,7 +46,7 @@ public class ListarPersonajesActivity extends AppCompatActivity {
         DatabaseUtils.dumpCursor(datos.obtenerPersonajes());
     }
 
-    public void btnCrearPersonaje() {
+    public void btnCrearPersonaje(View view) {
         Intent intent = new Intent(ListarPersonajesActivity.this, CrearPersonaje.class);
         startActivity(intent);
     }
